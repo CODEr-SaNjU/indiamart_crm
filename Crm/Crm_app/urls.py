0255b676169bc1a7cbaf15007a1a96cfed85904e
coder-sanju <sanju2help@gmail.com>
2020-11-06 11:49:12 +0530

    update  login page

--- Crm/Crm_app/urls.py
+++ Crm/Crm_app/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 from django.contrib.auth import views as auth_views
 urlpatterns = [
+    path('',views.login,name="login"),
 
 
 ]
--- Crm/Crm_app/views.py
+++ Crm/Crm_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+def login(request):
+    return render(request,"html_files/login.htm")
