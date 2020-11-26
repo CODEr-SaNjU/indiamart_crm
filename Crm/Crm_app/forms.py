ce721ab224d3a3f9d21b583bf853fec4eaa6b016
coder-sanju <sanju2help@gmail.com>
2020-11-27 02:45:26 +0530

    update show assigned enq

--- Crm/Crm_app/forms.py
+++ Crm/Crm_app/forms.py
@@ -10,7 +10,7 @@ from bootstrap_modal_forms.forms import BSModalModelForm
 class CK_AccountForm(BSModalModelForm):
     class Meta:
         model =CK_Account
-        fields = ['username','RN','QUERY_ID','QTYPE','SENDERNAME','SENDEREMAIL','SUBJECT','DATE_RE','DATE_R','DATE_TIME_RE','GLUSR_USR_COMPANYNAME','READ_STATUS','SENDER_GLUSR_USR_ID','MOB','QUERY_MODID','LOG_TIME','QUERY_MODREFID','DIR_QUERY_MODREF_TYPE','ORG_SENDER_GLUSR_ID','ENQ_MESSAGE','ENQ_ADDRESS','ENQ_CALL_DURATION','ENQ_RECEIVER_MOB','ENQ_CITY','ENQ_STATE','PRODUCT_NAME','COUNTRY_ISO','EMAIL_ALT','MOBILE_ALT','PHONE','PHONE_ALT','IM_MEMBER_SINCE','TOTAL_COUNT']
+        fields = ['username','RN','QUERY_ID','QTYPE','SENDERNAME','SENDEREMAIL','SUBJECT','DATE_RE','DATE_R','DATE_TIME_RE','GLUSR_USR_COMPANYNAME','READ_STATUS','SENDER_GLUSR_USR_ID','MOB','QUERY_MODID','LOG_TIME','QUERY_MODREFID','DIR_QUERY_MODREF_TYPE','ORG_SENDER_GLUSR_ID','ENQ_MESSAGE','ENQ_ADDRESS','ENQ_CALL_DURATION','ENQ_RECEIVER_MOB','ENQ_CITY','ENQ_STATE','PRODUCT_NAME','COUNTRY_ISO','EMAIL_ALT','MOBILE_ALT','PHONE','PHONE_ALT','IM_MEMBER_SINCE','TOTAL_COUNT','enquiry_source','expected_purchase_Date','profession','visit_date','visited_status','Visit_status','Booking_Date','remarks']
         widgets = {
           'RN' : forms.TextInput(attrs={'class':'form-control','id':'rnid'}),
           'QUERY_ID' : forms.TextInput(attrs={'class':'form-control','id':'queryid'}),
@@ -27,6 +27,7 @@ class CK_AccountForm(BSModalModelForm):
           'MOB' : forms.TextInput(attrs={'class':'form-control','id':'MOBid'}),
           'QUERY_MODID' : forms.TextInput(attrs={'class':'form-control','id':'QUERY_MODIDid'}),
           'GLUSR_USR_COMPANYNAMEid' : forms.TextInput(attrs={'class':'form-control','id':'GLUSR_USR_COMPANYNAMEid'}),
+
         }
         labels = {
             'username': ('Assign to user')
