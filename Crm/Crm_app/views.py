2dafa652cbddfbc693f242fe93278c8fdf9031ea
coder-sanju <sanju2help@gmail.com>
2020-11-28 01:50:37 +0530

    update

--- Crm/Crm_app/views.py
+++ Crm/Crm_app/views.py
@@ -60,13 +60,14 @@ def Admin_panel(request):
     all_enq = CK_Account.objects.all()
     all_user = User.objects.all()
     total_enquiry_data = all_enq.count()
-    # user = User.objects.all()
-    # total_user = user.count()
-    # superusers_count = User.objects.filter(is_superuser=True).count
+    assign_enq = CK_Account.objects.filter(username__isnull=False)
+    assign_enq_count = CK_Account.objects.filter(username__isnull=False).count()
+    notassign_enq = CK_Account.objects.filter(username__isnull=True)
+    notassign_enq_count = CK_Account.objects.filter(username__isnull=True).count()
     last_all_enq = CK_Account.objects.filter().order_by('-id')[:10]
     all_enq_in_ascending_order = reversed(last_all_enq)
 
-    return render(request,'html_files/Main.htm',{'last_all_enq':last_all_enq,'total_enquiry_data':total_enquiry_data,'all_enq':all_enq,'userform': userform, 'all_user':all_user})
+    return render(request,'html_files/Main.htm',{'last_all_enq':last_all_enq,'total_enquiry_data':total_enquiry_data,'all_enq':all_enq,'userform': userform, 'all_user':all_user,'assign_enq':assign_enq,'notassign_enq':notassign_enq,'assign_enq_count':assign_enq_count,'notassign_enq_count':notassign_enq_count})
 
 
 
